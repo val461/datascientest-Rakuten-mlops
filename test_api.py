0d8bc31830b36e80de22a3bf966f7af598f118a4
val461 <6820182+val461@users.noreply.github.com>
2026-06-04 15:50:56 +0200

    pytest: new tests for training lock and simultaneous requests
    changed deps of pytest venv: replaced requests with httpx; added pytest-asyncio

--- test_api.py
+++ test_api.py
@@ -2,9 +2,10 @@ from dotenv import load_dotenv
 load_dotenv()  # loads .env
 
 import pytest
-import requests
+import httpx
 import logging
 import os
+import asyncio
 
 logger = logging.getLogger(__name__)
 
@@ -23,10 +24,11 @@ def correct_payload():
 
 
 def test_health():
-    response = requests.get(f"{BASE_URL}/health")
+    response = httpx.get(f"{BASE_URL}/health")
     assert response.status_code == 200
     assert response.json()["status"] == "healthy"
     assert response.json()["service"] == "inference-api"
+    assert "training_ongoing" in response.json()
 
 
 def test_auth(correct_payload):
@@ -34,45 +36,63 @@ def test_auth(correct_payload):
     assert API_KEY is not None
 
     # try missing key
-    response = requests.post(f"{BASE_URL}/predict", json=correct_payload)
+    response = httpx.post(f"{BASE_URL}/predict", json=correct_payload)
     assert response.status_code == 401
-    response = requests.post(f"{BASE_URL}/train")
+    response = httpx.post(f"{BASE_URL}/train")
     assert response.status_code == 401
 
     # try wrong key
     wrong_key="wrong_key"
-    response = requests.post(f"{BASE_URL}/predict", headers={"X-API-Key": wrong_key}, json=correct_payload)
+    response = httpx.post(f"{BASE_URL}/predict", headers={"X-API-Key": wrong_key}, json=correct_payload)
     assert response.status_code == 403
-    response = requests.post(f"{BASE_URL}/train", headers={"X-API-Key": wrong_key})
+    response = httpx.post(f"{BASE_URL}/train", headers={"X-API-Key": wrong_key})
     assert response.status_code == 403
 
 
 def test_predict(correct_payload):
     # try correct request
-    response = requests.post(f"{BASE_URL}/predict", headers={"X-API-Key": API_KEY}, json=correct_payload)
+    response = httpx.post(f"{BASE_URL}/predict", headers={"X-API-Key": API_KEY}, json=correct_payload)
     assert response.status_code == 200
     data = response.json()
     assert "prediction" in data
 
     # try missing payload
-    response = requests.post(f"{BASE_URL}/predict", headers={"X-API-Key": API_KEY})
+    response = httpx.post(f"{BASE_URL}/predict", headers={"X-API-Key": API_KEY})
     assert response.status_code == 422
 
     # try wrong payload (type int instead of str; missing fields)
     wrong_payload = {"designation": 22}
-    response = requests.post(f"{BASE_URL}/predict", headers={"X-API-Key": API_KEY}, json=wrong_payload)
+    response = httpx.post(f"{BASE_URL}/predict", headers={"X-API-Key": API_KEY}, json=wrong_payload)
     assert response.status_code == 422
 
     # try wrong payload (empty value; missing fields)
     wrong_payload = {"designation": ""}
-    response = requests.post(f"{BASE_URL}/predict", headers={"X-API-Key": API_KEY}, json=wrong_payload)
+    response = httpx.post(f"{BASE_URL}/predict", headers={"X-API-Key": API_KEY}, json=wrong_payload)
     assert response.status_code == 200
     data = response.json()
     assert "prediction" in data
 
 
-def test_train():
+@pytest.mark.asyncio  # tag for compatibility with module pytest-asyncio
+async def test_async_requests(correct_payload):
+    n=5
+    # Make n prediction requests at nearly the same time. They are supposed to succeed.
+    async with httpx.AsyncClient() as client:
+        requests = [client.post(f"{BASE_URL}/predict", headers={"X-API-Key": API_KEY}, json=correct_payload) for _ in range(n)]
+        responses = await asyncio.gather(*requests)
+    assert len(responses) == n
+    for response in responses:
+        assert response.status_code == 200
+
+
+@pytest.mark.asyncio
+async def test_train():
     logger.info('May take 10mn.')
-    response = requests.post(f"{BASE_URL}/train", headers={"X-API-Key": API_KEY})
-    assert response.status_code == 200
-    assert response.json()["status"] == "success"
+    # Make 2 training requests at nearly the same time. Request 2 is supposed to be denied because a training lock was implemented.
+    async with httpx.AsyncClient() as client:
+        request1 = client.post(f"{BASE_URL}/train", headers={"X-API-Key": API_KEY}, timeout=None)
+        request2 = client.post(f"{BASE_URL}/train", headers={"X-API-Key": API_KEY}, timeout=None)
+        response1, response2 = await asyncio.gather(request1, request2)
+    assert response1.status_code == 200
+    assert response1.json()["status"] == "success"
+    assert response2.status_code == 409
