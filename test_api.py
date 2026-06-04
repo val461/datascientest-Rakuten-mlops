8806ed24b65ac4fb94216084a38e653fc5826209
val461 <6820182+val461@users.noreply.github.com>
2026-06-04 09:56:53 +0200

    pytest auth; add dep dotenv==0.9.9 to pytest venv

--- test_api.py
+++ test_api.py
@@ -1,10 +1,25 @@
+from dotenv import load_dotenv
+load_dotenv()  # loads .env
+
 import pytest
 import requests
 import logging
+import os
 
 logger = logging.getLogger(__name__)
 
 BASE_URL = "http://localhost:8000"
+API_KEY = os.getenv("API_KEY")
+
+
+@pytest.fixture
+def correct_payload():
+    return {
+        "designation": "Folkmanis Puppets - Marionnette Et Theatre - Mini Turtle",
+        "description": "Marionnette tortue miniature en tissu",
+        "productid": 516376098,
+        "imageid":1019294171
+    }
 
 
 def test_health():
@@ -14,14 +29,27 @@ def test_health():
     assert response.json()["service"] == "inference-api"
 
 
-def test_predict():
-    payload = {
-        "designation": "Folkmanis Puppets - Marionnette Et Theatre - Mini Turtle",
-        "description": "Marionnette tortue miniature en tissu",
-        "productid": 516376098,
-        "imageid":1019294171
-    }
-    response = requests.post(f"{BASE_URL}/predict", json=payload)
+def test_auth(correct_payload):
+    # check api key is loaded from .env by pytest
+    assert API_KEY is not None
+
+    # try missing key
+    response = requests.post(f"{BASE_URL}/predict", json=correct_payload)
+    assert response.status_code == 401
+    response = requests.post(f"{BASE_URL}/train")
+    assert response.status_code == 401
+
+    # try wrong key
+    wrong_key="wrong_key"
+    response = requests.post(f"{BASE_URL}/predict", headers={"X-API-Key": wrong_key}, json=correct_payload)
+    assert response.status_code == 403
+    response = requests.post(f"{BASE_URL}/train", headers={"X-API-Key": wrong_key})
+    assert response.status_code == 403
+
+
+def test_predict(correct_payload):
+    # try correct request
+    response = requests.post(f"{BASE_URL}/predict", headers={"X-API-Key": API_KEY}, json=correct_payload)
     assert response.status_code == 200
     data = response.json()
     assert "prediction" in data
@@ -29,7 +57,6 @@ def test_predict():
 
 def test_train():
     logger.info('May take 10mn.')
-    response = requests.post(f"{BASE_URL}/train")
+    response = requests.post(f"{BASE_URL}/train", headers={"X-API-Key": API_KEY})
     assert response.status_code == 200
     assert response.json()["status"] == "success"
-
