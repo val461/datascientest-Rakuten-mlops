6e93fc0fea10dadd735359938d8209a951c57572
val461 <6820182+val461@users.noreply.github.com>
2026-06-04 16:13:46 +0200

    pytest: bring back simple test_train function; improve logging

--- test_api.py
+++ test_api.py
@@ -8,6 +8,7 @@ import os
 import asyncio
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 BASE_URL = "http://localhost:8000"
 API_KEY = os.getenv("API_KEY")
@@ -73,6 +74,14 @@ def test_predict(correct_payload):
     assert "prediction" in data
 
 
+def test_train():
+    logger.info('May take 10mn.')
+    response = httpx.post(f"{BASE_URL}/train", headers={"X-API-Key": API_KEY}, timeout=None)
+    logger.debug(f"{response.text=}")
+    assert response.status_code == 200
+    assert response.json()["status"] == "success"
+
+
 @pytest.mark.asyncio  # tag for compatibility with module pytest-asyncio
 async def test_async_requests(correct_payload):
     n=5
@@ -86,13 +95,14 @@ async def test_async_requests(correct_payload):
 
 
 @pytest.mark.asyncio
-async def test_train():
+async def test_async_train():
     logger.info('May take 10mn.')
     # Make 2 training requests at nearly the same time. Request 2 is supposed to be denied because a training lock was implemented.
     async with httpx.AsyncClient() as client:
         request1 = client.post(f"{BASE_URL}/train", headers={"X-API-Key": API_KEY}, timeout=None)
         request2 = client.post(f"{BASE_URL}/train", headers={"X-API-Key": API_KEY}, timeout=None)
         response1, response2 = await asyncio.gather(request1, request2)
+    logger.debug(f"{response1.text=}\n{response2.text=}")
     assert response1.status_code == 200
     assert response1.json()["status"] == "success"
     assert response2.status_code == 409
